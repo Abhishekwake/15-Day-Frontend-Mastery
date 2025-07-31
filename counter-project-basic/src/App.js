ee732f7a581b1244769c5fdcdbd02061eef84bb2
Abhishek Wake <abhishekwake111@gmail.com>
2025-07-31 15:34:21 +0530

    Re-added counter project as a regular folder

---
+++ counter-project-basic/src/App.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+function App()
+{
+  const[user,setUser] =useState("");
+  useEffect(()=> {
+  //fake API call
+  //using timout to update user to abhishek after 2 sec
+  setTimeout(()=> {
+    setUser("Abhishek Wake")
+  },2000);
+  },[]);
+
+  return <h1>Hello {user}</h1>
+}
+export default App;
+
+
+/*
+import { useState } from "react";
+
+function App() {  
+  const[value,setValue] = useState(0);
+  return (
+    <div className="App">
+   <h1>{value}</h1>
+   <button onClick={()=> setValue(value+1)}>
+    Click Meh
+   </button>
+   </div>
+
+  );
+}
+export default App;
+*/
+
+//yeh jo app hai use hi component kehte hai
+//components are navbar,footers,cards,Buttos,forms
+// / All can be made reusable.
