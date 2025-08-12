8d829c857322f11d5e2a94ca7cb8f84c8ad3cc2d
Abhishek Wake <abhishekwake111@gmail.com>
2025-08-12 23:56:00 +0530

    Signup and login verfication token]

---
+++ auth-nextjs/src/models/userModel.js
@@ -0,0 +1,33 @@
+import { verify } from "crypto";
+import mongoose from "mongoose";
+import { type } from "os";
+const userSchema = new mongoose.Schema({
+    username : {
+        type: String,
+        required: [true, "Please Provide a username"],
+        unique: true,
+    },
+    email: {    
+        type: String,
+        required: [true, "Please Provide a email"],
+        unique: ture,
+    },
+    password: {
+        type: String,
+        required: [true,"Please provide a password"],
+    },
+    isVerified: {
+        type: Boolean,
+        default: false,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    forgotPasswordToken: String,
+    forgetPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date,
+})
+const User = mongoose.models.users || mongoose.model("users",userSchema);
+export default User;
